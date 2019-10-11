eecd62acfb32522c76731bbf271a74b75b1250b3
Finn <finnterdal@gmail.com>
2019-10-11 16:40:50 -0500

    staring some array method chaining tests

---
+++ chaining-1/test.js
@@ -0,0 +1,9 @@
+console.clear()
+const { expect } = require('chai')
+const chainmail = require('.')
+
+describe('Entries to Object', () => {
+  it('A', () => {})
+  it('B', () => {})
+  it('C', () => {})
+})
